0afb2e21d8d01386a302e5d71b2caccadc006a88
halimbagusalhakim <halsat132@gmail.com>
2025-11-20 12:55:44 +0700

    update import

--- app/src/main/java/com/example/praktikum7/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/praktikum7/viewmodel/SiswaViewModel.kt
@@ -2,9 +2,17 @@ package com.example.praktikum7.viewmodel
 
 import android.R
 import androidx.compose.runtime.mutableStateOf
+import com.example.praktikum7.model.Siswa
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import androidx.lifecycle.ViewModel
 
-class SiswaViewModel : viewmodel() {
-    private val _statusUI = mutableStateFlow(siswa())
+
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
 
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 
