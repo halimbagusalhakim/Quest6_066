68d30c23f300cc3635c453c92813fae45cc4dda2
halimbagusalhakim <halsat132@gmail.com>
2025-11-20 12:48:46 +0700

    membuat function set siswa dengan parameter ls

--- app/src/main/java/com/example/praktikum7/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/praktikum7/viewmodel/SiswaViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.praktikum7.viewmodel
 
+import android.R
 import androidx.compose.runtime.mutableStateOf
 
 class SiswaViewModel : viewmodel() {
     private val _statusUI = mutableStateFlow(siswa())
 
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls:MutableList<String>){
+
+    }
 
 
 
