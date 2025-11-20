bcd1bab133af5a6d08f452fdd54c7faae33ca114
halimbagusalhakim <halsat132@gmail.com>
2025-11-20 12:24:05 +0700

    menambahkan gradle navigation compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
