bd99c35a77a5fb540c42eb5a42bfe62a99d45daf
halimbagusalhakim <halsat132@gmail.com>
2025-11-20 12:34:06 +0700

    menambahkan untuk variabel alamat

--- app/src/main/java/com/example/praktikum7/model/Siswa.kt
+++ app/src/main/java/com/example/praktikum7/model/Siswa.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum7.model
 
 data class Siswa(
-    val nama: String="",
-    val gender : String=""
+    val nama: String ="",
+    val gender : String ="",
+    val alamat : String =""
 )
